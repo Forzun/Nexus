6872a82196a53bd64c16ebfe2106e28eb0d69107
Forzun <vbhavesh219@gmail.com>
2025-10-19 09:48:41 +0530

    productFooter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { HeroHeader } from "@/components/header";
 import HeroSection from "@/components/hero-section";
 import FeaturesSection from "@/components/ui/feature";
+import FooterSection from "@/components/ui/footer";
 import Pricing from "@/components/ui/pricing";
 import Image from "next/image";
 
@@ -11,6 +12,7 @@ export default function Home() {
         <HeroSection />
         <FeaturesSection />
         <Pricing />
+        <FooterSection />
     </div>
   );
 }
---
+++ components/ui/footer.tsx
@@ -0,0 +1,119 @@
+import { Logo } from "@/components/logo";
+import Link from "next/link";
+
+const links = [
+  {
+    title: "Features",
+    href: "#features",
+  },
+  {
+    title: "Customers",
+    href: "/customers",
+  },
+  {
+    title: "Pricing",
+    href: "Pricing",
+  },
+  {
+    title: "Help",
+    href: "#help",
+  },
+  {
+    title: "About",
+    href: "#about",
+  },
+];
+
+export default function FooterSection() {
+  return (
+    <footer className="py-16 md:py-32">
+      <div className="mx-auto max-w-5xl px-6">
+        <Link href="/" aria-label="go home" className="mx-auto block size-fit">
+          <Logo />
+        </Link>
+
+        <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
+          {links.map((link, index) => (
+            <Link
+              key={index}
+              href={link.href}
+              className="text-muted-foreground hover:text-primary block duration-150"
+            >
+              <span>{link.title}</span>
+            </Link>
+          ))}
+        </div>
+        <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
+          <Link
+            href="https://x.com/ForZun_"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="X/Twitter"
+            className="text-muted-foreground hover:text-primary block"
+          >
+            <svg
+              className="size-6"
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.32 21h2.65zM16.25 19L5.75 5h2l10.5 14z"
+              ></path>
+            </svg>
+          </Link>
+          <Link
+            href="https://github.com/forzun"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Github"
+            className="text-muted-foreground hover:text-primary block"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              color="currentColor"
+              fill="none"
+            >
+              <path
+                d="M6.51734 17.1132C6.91177 17.6905 8.10883 18.9228 9.74168 19.2333M9.86428 22C8.83582 21.8306 2 19.6057 2 12.0926C2 5.06329 8.0019 2 12.0008 2C15.9996 2 22 5.06329 22 12.0926C22 19.6057 15.1642 21.8306 14.1357 22C14.1357 22 13.9267 18.5826 14.0487 17.9969C14.1706 17.4113 13.7552 16.4688 13.7552 16.4688C14.7262 16.1055 16.2043 15.5847 16.7001 14.1874C17.0848 13.1032 17.3268 11.5288 16.2508 10.0489C16.2508 10.0489 16.5318 7.65809 15.9996 7.56548C15.4675 7.47287 13.8998 8.51192 13.8998 8.51192C13.4432 8.38248 12.4243 8.13476 12.0018 8.17939C11.5792 8.13476 10.5568 8.38248 10.1002 8.51192C10.1002 8.51192 8.53249 7.47287 8.00036 7.56548C7.46823 7.65809 7.74917 10.0489 7.74917 10.0489C6.67316 11.5288 6.91516 13.1032 7.2999 14.1874C7.79575 15.5847 9.27384 16.1055 10.2448 16.4688C10.2448 16.4688 9.82944 17.4113 9.95135 17.9969C10.0733 18.5826 9.86428 22 9.86428 22Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></path>
+            </svg>
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/bhavesh-mandawriya-981a4228b/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+            className="text-muted-foreground hover:text-primary block"
+          >
+            <svg
+              className="size-6"
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
+              ></path>
+            </svg>
+          </Link>
+        </div>
+        <span className="text-muted-foreground block text-center text-sm">
+          {" "}
+          © {new Date().getFullYear()} Nexus
+        </span>
+      </div>
+    </footer>
+  );
+}
